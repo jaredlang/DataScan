83eb014fd3c7b60002de54af15703db88422cd3e
jaredlang <jaredlang76@gmail.com>
2017-12-08 20:40:18 -0600

    added an option to create batch copy

--- make_mxd_update_batch.py
+++ make_mxd_update_batch.py
@@ -3,7 +3,9 @@ import sys
 import argparse
 from datetime import datetime
 
-CMD = "python H:\MXD_Scan\update_mxds.py -a single "
+UPDATE_CMD = "python H:\MXD_Scan\update_mxds.py -a single "
+COPY_CMD = "copy /B /Y "
+
 SRC_FOLDER = r"\\anadarko.com\world\SharedData\Houston\IntlDeepW\MOZAMBIQUE\MOZGIS\arcgis_files\mxds"
 TGT_FOLDER = r"H:\MXD_Scan\xlsx\arcgis_files\mxds_updateDDD_all"
 XTS_FOLDER = r"H:\MXD_Scan\xlsx\arcgis_files\mxds_load_all"
@@ -24,6 +26,22 @@ def make_batch_cmd(exeCmd, srcFile, tgtFile, xtsFile, cmdFile):
                             xtsLine.replace('\n', '')]) + "\n")
 
 
+def make_copy_cmd(srcPath):
+    tgtPath = srcPath.replace(SRC_FOLDER, TGT_FOLDER)
+
+    return "%s %s %s\n" % (COPY_CMD, srcPath, tgtPath)
+
+
+def make_batch_update_cmd(srcFile, batFile):
+
+    with open(batFile, "w") as bat:
+        with open(srcFile, "r") as src:
+            srcPaths = src.readlines()
+            for srcPath in srcPaths:
+                updateCmd = make_update_cmd(srcPath.replace('\n', ''))
+                bat.write(updateCmd)
+
+
 def make_update_cmd(srcPath):
     tgtPath = srcPath.replace(SRC_FOLDER, TGT_FOLDER)
     xtsPath = srcPath.replace(SRC_FOLDER, XTS_FOLDER)
@@ -31,7 +49,7 @@ def make_update_cmd(srcPath):
         xtsPath = xtsPath[:-1]
     xtsPath = xtsPath + ".xlsx"
 
-    return "%s -m %s -n %s -x %s\n" % (CMD, srcPath, tgtPath, xtsPath)
+    return "%s -m %s -n %s -x %s\n" % (UPDATE_CMD, srcPath, tgtPath, xtsPath)
 
 
 def make_batch_update_cmd(srcFile, batFile):
@@ -49,11 +67,19 @@ if __name__ == '__main__':
 
     parser.add_argument('-s','--src', help='A file containing a list of mxd file paths', required=True)
     parser.add_argument('-b','--bat', help='Command batch file (output)', required=True)
+    parser.add_argument('-a','--action', help='Action Options (update, copy)', required=False, default='update')
 
     params = parser.parse_args()
 
-    make_batch_update_cmd(params.src, params.bat)
-    print('###### Completed making mxd update batch [%s]'%params.bat)
+    if params.action == "update":
+        make_batch_update_cmd(params.src, params.bat)
+        print('###### Completed making mxd update batch [%s]'%params.bat)
+    elif params.action == "copy":
+        make_batch_copy_cmd(params.src, params.bat)
+        print('###### Completed making mxd copy batch [%s]'%params.bat)
+    else:
+        print 'Error: unknown action [%s] for importing' % params.action
+
 
 '''
     parser.add_argument('-s','--src', help='Source parameter file (input)', required=True)
