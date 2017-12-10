68d17527d9ea8af07d9c49d70a3174c375794ec1
jaredlang <jaredlang76@gmail.com>
2017-12-10 12:15:27 -0600

    auto generated the batch cmds for missed files.
    dynamically generated the batch cmds for missed files.

--- batch_scan_load_missed.cmd
+++ batch_scan_load_missed.cmd
@@ -3,10 +3,16 @@ echo SCAN+LOAD (Missed) script START %date% %time%
 
 rmdir /s /q H:\MXD_Scan\xlsx\arcgis_files\mxds
 
-echo scan mxd of Missed %date% %time%
-call H:\MXD_Scan\xlsx\arcgis_files\scan_mxds_missed.cmd > H:\MXD_Scan\xlsx\arcgis_files\scan_mxds_missed.log 2>&1
+echo find missed mxds %date% %time%
+python H:\MXD_Scan\find_missed_mxds.py -t xlsx -x H:\MXD_Scan\xlsx\arcgis_files\mxds_scan_all -m \\anadarko.com\world\SharedData\Houston\IntlDeepW\MOZAMBIQUE\MOZGIS\arcgis_files\mxds > H:\MXD_Scan\xlsx\arcgis_files\scan_mxds_missed.txt 2>&1
 
-xcopy H:\MXD_Scan\xlsx\arcgis_files\mxds\* H:\MXD_Scan\xlsx\arcgis_files\mxds_scan_missed\ /c /e /y /q
+echo make scan cmd for Missed mxds %date% %time%
+python H:\MXD_Scan\make_mxd_batch_cmd.py -a scan -b H:\MXD_Scan\xlsx\arcgis_files\batch_scan_missed_mxds.cmd -s H:\MXD_Scan\xlsx\arcgis_files\scan_mxds_missed.txt
+
+echo scan mxd of Missed %date% %time%
+call H:\MXD_Scan\xlsx\arcgis_files\batch_scan_missed_mxds.cmd > H:\MXD_Scan\xlsx\arcgis_files\scan_mxds_missed.log 2>&1
+
+xcopy H:\MXD_Scan\xlsx\arcgis_files\mxds_scan_missed\* H:\MXD_Scan\xlsx\arcgis_files\mxds\ /c /e /y /q
 xcopy H:\MXD_Scan\xlsx\arcgis_files\mxds_scan_missed\* H:\MXD_Scan\xlsx\arcgis_files\mxds_scan_all\ /c /e /y /q
 
 echo load data of Missed %date% %time%
--- batch_scan_load_update.cmd
+++ batch_scan_load_update.cmd
@@ -6,16 +6,14 @@ python H:\MXD_Scan\find_missed_mxds.py -t xlsx -x H:\MXD_Scan\xlsx\arcgis_files\
 
 python H:\MXD_Scan\find_missed_mxds.py -t xlsx -x H:\MXD_Scan\xlsx\arcgis_files\mxds_load_all -m \\anadarko.com\world\SharedData\Houston\IntlDeepW\MOZAMBIQUE\MOZGIS\arcgis_files\mxds > H:\MXD_Scan\xlsx\arcgis_files\load_mxds_missed_NEW.log 2>&1
 
-REM call H:\MXD_Scan\compile_data_folders.cmd
-
 call H:\MXD_Scan\batch_update_mxds.cmd
 
 python H:\MXD_Scan\find_missed_mxds.py -t mxd -x H:\MXD_Scan\xlsx\arcgis_files\mxds_updateDDD_all -m \\anadarko.com\world\SharedData\Houston\IntlDeepW\MOZAMBIQUE\MOZGIS\arcgis_files\mxds > H:\MXD_Scan\xlsx\arcgis_files\update_mxds_missed_files_NEW.txt 2>&1
 
-python H:\MXD_Scan\make_mxd_update_batch.py -b H:\MXD_Scan\batch_update_missed_mxds.cmd -s H:\MXD_Scan\xlsx\arcgis_files\update_mxds_missed_files_NEW.txt
+python H:\MXD_Scan\make_mxd_batch_cmd.py -a update -b H:\MXD_Scan\xlsx\arcgis_files\batch_update_missed_mxds.cmd -s H:\MXD_Scan\xlsx\arcgis_files\update_mxds_missed_files_NEW.txt
 
 echo MXD(missed) UPDATE script START %date% %time%
-call H:\MXD_Scan\batch_update_missed_mxds.cmd > H:\MXD_Scan\xlsx\arcgis_files\update_mxds_missed.log 2>&1
+call H:\MXD_Scan\xlsx\arcgis_files\batch_update_missed_mxds.cmd > H:\MXD_Scan\xlsx\arcgis_files\update_mxds_missed.log 2>&1
 echo MXD(missed) UPDATE script END %date% %time%
 
 python H:\MXD_Scan\find_missed_mxds.py -t mxd -x H:\MXD_Scan\xlsx\arcgis_files\mxds_updateDDD_all -m \\anadarko.com\world\SharedData\Houston\IntlDeepW\MOZAMBIQUE\MOZGIS\arcgis_files\mxds > H:\MXD_Scan\xlsx\arcgis_files\update_mxds_missed_files_NEW2.txt 2>&1
